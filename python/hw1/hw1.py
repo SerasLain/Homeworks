33095d842ed3d2cb14486facba626bbe8c9bda57
Эйко Нян < >
2016-10-15 13:13:27 +0300

    созданы отдельные папки для КИЛИ и питона, добавлена первая домашка по питону

---
+++ python/hw1/hw1.py
@@ -0,0 +1,12 @@
+print('введите числа')
+a = int(input())
+b = int(input())
+c = int(input())
+if a%b == c :
+    print('с - отстаток от деления a на b')
+else:
+    print ('c не является остатком от деления a на b')
+if a/b == c:
+    print ('с - частное от деления a на b')
+else:
+    print ('с не является частным от деления a на b')
