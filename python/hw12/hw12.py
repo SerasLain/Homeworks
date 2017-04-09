4b5b48960f7e51920bc9ade902cc2e2cef1e10a3
SerasLain <seraslain@gmail.com>
2017-04-09 21:51:22 +0300

    домашка 12 (про format и comprehensions)

--- python/hw12/hw12.py
+++ python/hw12/hw12.py
@@ -5,7 +5,8 @@ def opentext():
     return text
 
 
-def makesent(text):
+def makesent(text): 
+    ## расставляет символ-разделитель предложений
     text = text.replace('.','.*')
     text = text.replace('?', '?*')
     text = text.replace('!', '!*')
@@ -13,17 +14,20 @@ def makesent(text):
 
 
 def sentencer(text):
+    ## создает массив из предложений с сохранением всей пунктуации
     sentarr = [sentence for sentence in text.split('*')]
     return sentarr
 
 
 def medlength(sentence):
+    ##считает среднюю длину слов в предложении
     arr = [len(word.strip('./",:;-—()!?')) for word in sentence.split()]
     med = round(sum(arr)/len(sentence.split()), 1)
     return med
 
 
 def dictsentlen(sentarr):
+    ##создает словарь вида предложение: средняя длина слова в предложении для предложений длиннее десяти слов
     dictionary = {sentence: medlength(sentence) for sentence in sentarr if len(sentence.split()) > 10}
     return dictionary
 
